438e66781d997ee6994ee6fbed070b7476f4ca08
FunnyDogLol <132663211+FunnyDogLol@users.noreply.github.com>
2023-12-26 18:49:10 +0000

    Create day4p2.py

---
+++ day4/day4p2.py
@@ -0,0 +1,41 @@
+slines = []
+with open("valss", "r") as file:
+    slines = file.readlines()
+
+wlines = []
+with open("valsw", "r") as file:
+    wlines = file.readlines()
+
+for i in range(len(slines)):
+    wlines[i] = wlines[i].split()
+    slines[i] = slines[i].split()
+
+ans = 0
+def findwins(scratch: list, wins: list, num: int) -> list:
+    global ans
+    ans += 1
+    ret = []
+    newnum = num
+    for i in scratch:
+        if i in wins:
+            newnum += 1
+            ret.append(newnum)
+
+    return ret
+
+
+def recfunc(scratchlines, winlines, num):
+    global ans
+
+    try:
+        for val in findwins(scratchlines[num], winlines[num], num + 1):
+            recfunc(scratchlines, winlines, val - 1)
+    except:
+       pass
+
+    return
+
+for i in range(len(slines)):
+    recfunc(slines, wlines, i)
+
+print(ans)
