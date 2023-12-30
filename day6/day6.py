df13bb3427e5ce5e857e7ef4ae045737a0005c33
FunnyDogLol <132663211+FunnyDogLol@users.noreply.github.com>
2023-12-30 17:28:47 +0000

    Create day6.py

---
+++ day6/day6.py
@@ -0,0 +1,22 @@
+with open(r"times") as file:
+    times = file.readlines()
+for i in range(2):
+    times[i] = times[i].strip().split()
+    times[i].pop(0)
+print(times)
+
+wcount = [0] * len(times[i])
+for i, rtime in enumerate(times[0]):
+    ptime = 0
+    while ptime < int(rtime):
+        ptime += 1
+        distance = ptime * (int(rtime) - ptime)
+        print(ptime, distance)
+        if distance > int(times[1][i]):
+            wcount[i] += 1
+
+ans = 1
+for n in wcount:
+    ans *= n
+
+print(ans)
